898643a8e80da07529a584bb4d6a992f356ea340
Iayoo <vilnius94@163.com>
2021-11-17 15:43:23 +0800

    修改晚上配置文件

--- config/open_api.php
+++ config/open_api.php
@@ -4,14 +4,16 @@ return [
 
     /*
     * --------------------------------------------------------------------------
-    * Cross-Origin Resource Sharing (CORS) Configuration
+    * Open-Api Configuration
     * --------------------------------------------------------------------------
     */
-
+    // 应用数据模型
     'db_model' => '',
-
+    // 多应用/但应用
     'app_mode' => 'multi',
+    // 是否校验时间戳
     'verify_timestamp' => true,
+    // 时间戳有效时长(单位:微秒)
     'timestamp_expire' => 120*1000,
 
 ];
